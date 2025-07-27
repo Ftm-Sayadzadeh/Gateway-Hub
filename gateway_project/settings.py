13a6f29888a48443fbc5ead640ff9c5f021262fb
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2025-07-27 14:08:02 +0330

    fix: fix static root in settings.py

--- gateway_project/settings.py
+++ gateway_project/settings.py
@@ -107,7 +107,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Directory where static files will be collected
+# CORRECTED: STATIC_ROOT should be a separate directory for collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Changed from 'static' to 'staticfiles'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gateway_manager', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
