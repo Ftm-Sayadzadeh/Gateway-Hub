8e1a83e68dcc95bf1f7d6efba2806e998161e7ad
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2025-09-15 12:12:01 +0330

    fix(layout): adjust main content margin for collapsed sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="flex">
-    <Sidebar />
+    <Sidebar :is-collapsed="isSidebarCollapsed" @toggle-collapse="isSidebarCollapsed = !isSidebarCollapsed" />
 
-    <main class="flex-1 ml-64">
+    <main 
+      :class="['flex-1 transition-all duration-300', isSidebarCollapsed ? 'ml-20' : 'ml-64']"
+    >
       <router-view />
     </main>
   </div>
 </template>
 
 <script>
-import Sidebar from '@/components/Sidebar.vue'
+import { ref } from 'vue';
+import Sidebar from '@/components/Sidebar.vue';
 
 export default {
   name: 'App',
   components: {
     Sidebar
+  },
+  setup() {
+    const isSidebarCollapsed = ref(false);
+    
+    return {
+      isSidebarCollapsed
+    };
   }
-}
+};
 </script>
 
 <style src="./colors.css"></style>
